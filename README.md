38cc6d7e01ba5eec199cb1f7d36d75e4bb38c00a
Schwark Satyavolu <schwark@alum.rpi.edu>
2021-01-02 22:51:09 -0800

    first check in

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pyunifi
+
+Python 2.x implementation the UniFi controller API. Will be progressively adding functionality - only basic calls supported at the moment
---
+++ pyunifi/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyunifi.
+
+A Python 2.x implementation of the UniFi controller API.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Schwark Satyavolu'
+__credits__ = 'Inspired by Art-of-WiFi UniFi-API-client in PHP'
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
